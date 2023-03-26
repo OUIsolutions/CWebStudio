6dd3af871adb551d5d19a5f00265ff114214ffba
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-26 14:04:19 -0300

    at

--- CWebStudio/request_interpretation.c
+++ CWebStudio/request_interpretation.c
@@ -2,21 +2,30 @@
 void interpret_raw_entrys(char *raw_entrys){
     //splite lines by "\r\n"
     struct DtwStringArray *lines = dtw_constructor_string_array();
-    int i;
-    for (i = 0; i < strlen(raw_entrys); i++){
-        if (raw_entrys[i] == '\r' && raw_entrys[i+1] == '\n')
-        {
-            raw_entrys[i] = '\0';
-            lines->add_string(lines, raw_entrys);
-            raw_entrys = raw_entrys + i + 2;
-            i = 0;
+
+    size_t raw_entrys_size = strlen(raw_entrys);
+    char last_string[1000]= "";
+    int line_index = 0;
+    for (int i = 0; i < raw_entrys_size; i++){
+        if (raw_entrys[i] == '\r' && raw_entrys[i+1] == '\n'){
+            last_string[line_index] = '\0';
+            lines->add_string(lines, last_string);
+            line_index=0;
+            i++;
         }
+        else{
+            last_string[line_index] = raw_entrys[i];
+            line_index++;
+        }    
     }
-    //extract the last num of lines
-    for (int i = 0; i < lines->size; i++){
-        puts("---------------------------------");
+    puts("---Original---"); 
+    printf("%s\n", raw_entrys);
+    puts("---Geracao de linhas concluida---");
+    for(int i = 0; i < lines->size; i++){
+        puts("----------------------");
         printf("%s\n", lines->strings[i]);
     }
+    
+    lines->free_string_array(lines);
 
-    puts("END OF LINES");
 }
