c19f7096d417c49becac05420bed77fcb8782d53
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-27 17:25:05 -0300

    creating headders

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
         // Enviando uma resposta HTTP ao cliente
         request->represent(request);
-
+        dtw_write_string_file_content("saida.txt", request->raw_entrys);
         //dtw_write_string_file_content("saida.txt", raw_entrys);
         return cew_send_file("captura1.png", CWEB_AUTO_SET_CONTENT, CWEB_OK);
 }
