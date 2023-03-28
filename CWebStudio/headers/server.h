b5939738b3950f586133b8835ee1b5ca3686b0b3
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-28 05:54:07 -0300

    att

--- CWebStudio/headers/server.h
+++ CWebStudio/headers/server.h
@@ -1,7 +1,16 @@
 
 static size_t actual_request = 0;
+
+#ifdef __cplusplus
+void cweb_run_sever(
+    int port,
+    struct CwebHttpResponse*(*request_handle)( struct CwebHttpRequest *request),
+    int timeout=30
+);
+#else
 void cweb_run_sever(
     int port,
     struct CwebHttpResponse*(*request_handle)( struct CwebHttpRequest *request),
     int timeout
 );
+#endif
