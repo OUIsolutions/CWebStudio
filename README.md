84c4b84509412894ea49e11f9573f5933a0b4745
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-29 19:43:30 -0300

    att

--- README.md
+++ README.md
@@ -364,3 +364,26 @@ int main(){
    return 0;
 }
 ~~~
+
+### CWEB_TIMEOUT 
+you can set the max timeout your aplication will deal, if is the function takes more 
+than that time , the sever will return 500
+~~~c 
+
+#define CWEB_DEBUG
+//set the time to 5 seconds
+#define CWEB_TIMEOUT 5
+#include "CWebStudio.c"
+struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
+   //it will return 500 on these aplication
+   sleep(10);
+   return cweb_send_text("Hello World", 200);
+}
+
+int main(){
+
+   cweb_run_server(5000, main_sever); 
+
+   return 0;
+}
+~~~
--- main.c
+++ main.c
@@ -1,22 +1,17 @@
 
 #define CWEB_DEBUG
-
+//set the time to 10 seconds
+#define CWEB_TIMEOUT 5
 #include "CWebStudio.c"
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
-
-    if(strcmp(request->route, "/test") == 0){
-        ///making an error 
-        int x = 1/0;
-        //Your application will crash here if you are using single process 
-        //because the error is not handled
-    }
-    return cweb_send_text("Hello World", 200);
-    
+   //it will return 500 on these aplication
+   sleep(10);
+   return cweb_send_text("Hello World", 200);
 }
 
 int main(){
 
-   cweb_run_server(5001, main_sever); 
+   cweb_run_server(5000, main_sever); 
 
    return 0;
 }
