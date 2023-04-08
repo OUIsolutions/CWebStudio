b7cb61566c9780bd3da35817042049582c1a3663
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-08 14:44:21 -0300

    at

--- main.c
+++ main.c
@@ -6,9 +6,18 @@
 
 
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
-   char teste[5];
-   return cweb_send_text("Hello World", 200);
+   char *a = request->params->get_value(request->params, "a");
+   char *b = request->params->get_value(request->params, "b");
+   if(a == NULL || b == NULL){
+       return cweb_send_text("Error: a and b are required", 400);
+   }
+   int a_int = atoi(a);
+   int b_int = atoi(b);
+   char *result = malloc(100);
+   sprintf(result, "value is %d", a_int + b_int);
+   return cweb_send_text(result, 200);
+
 
 }
 
-CWEB_START_MACRO(5005, main_sever);
+CWEB_START_MACRO(5000, main_sever);
