345d07f803a33ee5974f8a5d334551c0df53d7bc
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-13 10:23:15 -0300

    atat

--- CWebStudio.c
+++ CWebStudio.c
@@ -7166,7 +7166,7 @@ struct CwebHttpResponse* cweb_send_file(const char *file_path,const char *conten
         strcpy(content_type_created, content_type);
     }
 
-    return cweb_send_any(content_type_created, size+1000, content, status_code);
+    return cweb_send_any(content_type_created, size+10000, content, status_code);
     
 }
 
