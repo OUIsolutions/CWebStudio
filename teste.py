a4efd730beb959ea50df4b6fd44826e21a25cbaf
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-15 23:39:22 -0300

    at

--- teste.py
+++ teste.py
@@ -1,6 +1,16 @@
 import requests
-import base64
+
 
 adress = 'http://15.229.42.116/set'
 
 #send the bird to the adres
+
+def sendBird(bird:bytes):
+    #send directly the bytes
+    #convert to base64
+    #send the base64
+    requests.get(adress, data='aaaa')
+    print('request executed')
+with open('bird.png', 'rb') as f:
+    bird = f.read()
+    sendBird(bird)
