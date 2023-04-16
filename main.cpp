4e96ec0b124cfecaa814222f2cd9124791785fa9
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-15 23:26:16 -0300

    at

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-
-
 #define CWEB_DEBUG
 #include "CWebStudio/CwebStudioMain.c"
 
@@ -13,19 +11,15 @@ struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
     unsigned char *body = request->content;
     int size = request->content_length;
     std::string rota = request->route;
-    
     if(rota == "/get"){
-       return cweb_send_file("bird.png",CWEB_AUTO_SET_CONTENT,200);
+       return cweb_send_file("PySchema.png",CWEB_AUTO_SET_CONTENT,200);
     }
-    
-    if(rota == "/set"){        
-            //transform body from bas64 to binary
-            size_t out_size = 0;
-            unsigned char *binary = dtw_base64_decode((char*)body,size,&out_size);
-            dtw_write_any_content("teste.png",binary,out_size);
-            return cweb_send_text("uploaded",200);
-    }
+    if(rota == "/set"){
 
+            dtw_write_any_content("test.png", body, size);
+            return cweb_send_text("uploaded",200);
+
+    }
     else{
         return cweb_send_text("rota invalda",200);
     }
@@ -39,7 +33,7 @@ int main(){
         80,
         main_sever,
         20,
-        3000000,
+        1000000,
         false
     );
 }
