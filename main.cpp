0c1ef2c0f85c31c8b301cdafc1dfcc703ce0d3bd
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-15 23:28:14 -0300

    at

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@
 
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
 
-    dtw_write_any_content("raw",(unsigned char*)request->raw_entrys,50000);
     unsigned char *body = request->content;
     int size = request->content_length;
+
     std::string rota = request->route;
+    
     if(rota == "/get"){
        return cweb_send_file("PySchema.png",CWEB_AUTO_SET_CONTENT,200);
     }
