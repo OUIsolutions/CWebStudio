ff4ad607ff5f5d2acf3a1b5a1cbfe1e18f365a69
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-20 01:11:01 -0300

    fixed bugs

--- teste.c
+++ teste.c
@@ -1,30 +1,17 @@
-
 #include "CWebStudio.c"
 
+
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
-    printf("-------------------\n");
-    printf("size:%d\n",request->content_length);
-    request->represent(request);
-    
-    char *tag = request->get_header(request,"tag");
-    if(tag == NULL){
-        return cweb_send_text("not passed tag",200);
-    }
 
-    if(strcmp(tag,"teste") == 0){
-        return cweb_send_text("tag ok",200);
+    struct CwebDict *query_paramns = request->params;
+    for(int i = 0; i < query_paramns->size; i++){
+        struct CwebKeyVal *key_val = query_paramns->keys_vals[i];
+        char *key = key_val->key;
+        char *value = key_val->value;
+        printf("%s : %s\n", key, value);
     }
+    return cweb_send_text("Hello World", 200);
     
-    return cweb_send_text("tag not ok",200);
 }
 
-int main(int argc, char *argv[]){
-
-    cweb_run_server(
-        8080,
-        main_sever,
-        2,
-        CWEB_DEFAULT_MAX_BODY,
-        CWEB_SAFTY_MODE
-    );
-}
+CWEB_START_MACRO(5001, main_sever);
