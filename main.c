e6f6dab49e935a8fc31353812f1ad6e22ddb72a4
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-21 05:24:49 -0300

    at

--- main.c
+++ main.c
@@ -1,43 +1,37 @@
-#define CWEB_ONCE
+#define CWEB_DEBUG
 #include "CWebStudio/CwebStudioMain.c"
 
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
 
-    //request->represent(request);
+    char *route = request->route;
+    char *name = request->get_param(request, "name");
 
-    return cweb_send_text("Hello World33", 200);
+    if(name == NULL){
+        return cweb_send_text("name is required",400);
+    }
+
+    if(strcmp(route, "/get") == 0){
+        return cweb_send_file(name,CWEB_AUTO_SET_CONTENT,200);
+    }
+
+    else if(strcmp(route, "/set") == 0){
+        dtw_write_any_content(name,request->content,request->content_length);
+        return cweb_send_text("uploaded",200);
+    }
     
+    else{
+        return cweb_send_text("not route passed",404);
+    }
+
 }
 
-int main(){
- 
-    
-    char *teste = "Teste $ ";
-    char *normalized = cweb_normalize_string(teste,"$ ");
-    printf("Normalized:%s",normalized);
-    /*
+int main(int argc, char *argv[]){
+
     cweb_run_server(
         8080,
         main_sever,
         CWEB_DEFAULT_TIMEOUT,
         CWEB_DEFAULT_MAX_BODY,
-        CWEB_DANGEROUS_SINGLE_PROCESS
-
-        );
-    
-    
-    /*
-    struct CwebHttpRequest *request = cweb_request_constructor();
-     request->interpret_first_line(request,"GET /teste2");
-     struct DtwStringArray *headers = dtw_constructor_string_array();
-     headers->add_string(headers,"Teste : aaaaa");
-     headers->add_string(headers,"teste2: wwwww");
-     headers->add_string(headers,"teste3: wwwww");
-
-     int result = request->interpret_headders(request,headers);
-
-     printf("Result:%d",result);
-    */
-
-     return 0;
+        CWEB_SAFTY_MODE
+    );
 }
