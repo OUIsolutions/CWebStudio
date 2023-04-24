6194a6cc39a0e4a7b1ef3b95380bf823a1c3e5e5
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-24 19:39:14 -0300

    att

--- test.c
+++ test.c
@@ -1,15 +1,21 @@
-#define CWEB_ONCE
+
 #define OUI_DEBUG
 
 #include "CWebStudio/CwebStudioMain.c"
 
 
-
+int executions = 0;
 
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
+    
+    if(executions ==3){
+        request->free(request);
+       exit(0);
+    }
     char *test = (char*)malloc(100);
     sprintf(test, "Hello world");
-    
+    executions++;
+
     request->represent(request);
     
     return cweb_send_var_html_cleaning_memory(test, CWEB_OK);
@@ -20,7 +26,7 @@ int main(){
     
     
       cweb_run_server(
-        8081,
+        8080,
         main_sever,
         CWEB_DEFAULT_TIMEOUT,
         CWEB_DEFAULT_MAX_BODY,
