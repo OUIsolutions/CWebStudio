b576ed6aaef0fe4b25c9a812fbf7ca098af0a5f4
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-24 15:36:34 -0300

    at

--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 #define CWEB_ONCE
-
+#define OUI_DEBUG
+#define CWEB_DEBUG
 #include "CWebStudio/CwebStudioMain.c"
 
 
@@ -7,17 +8,17 @@
 
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
     
-    /*
+    
     char *test = (char*)malloc(100);
     strcpy(test,"Hello World!");
-    */
-    return cweb_send_text("aaaaa",CWEB_OK);
+    
+    return cweb_send_text_cleaning_memory("aaaaa",CWEB_OK);
     
 }
 
 int main(){
     
-    /*
+    
       cweb_run_server(
         8080,
         main_sever,
@@ -25,9 +26,6 @@ int main(){
         CWEB_DEFAULT_MAX_BODY,
         CWEB_DANGEROUS_SINGLE_PROCESS
     );
-    */
-   struct CwebHttpRequest *request = cweb_request_constructor();
-   request->interpret_first_line(request,"GET /aaa?teste=20 HTTP/1.1");
-   request->represent(request);
-   request->free(request);
+    
+
 }
