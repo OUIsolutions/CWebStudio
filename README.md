a80a5db4ffdd7aa98fc3f003aff252420837a9f5
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-26 14:54:59 -0300

    at

--- README.md
+++ README.md
@@ -216,7 +216,22 @@ struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
 
 CWEB_START_MACRO(5001, main_sever);
 ~~~
+Or if you have memory alocated in string, dont worry, just call 
+**cweb_send_text_cleaning_memory**
+~~~c
+#include "CWebStudio/CwebStudioMain.h"
 
+struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
+
+    char *teste = malloc(100);
+    strcpy(teste, "Hello World");
+    return cweb_send_text_cleaning_memory(teste,200);
+}
+
+CWEB_START_MACRO(5000, main_sever)
+
+
+~~~
 ## Other Formats 
 You can return other formats like these 
 ~~~c
--- main.c
+++ main.c
@@ -3,32 +3,10 @@
 
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
 
-    struct CTextStack *s  = newCTextStack(CTEXT_LINE_BREAKER,CTEXT_SEPARATOR);
-  
-    s->$open(s,HTML,"lang=\"%s\"","en");
-        s->open(s,HEAD);
-        
-        s->close(s,HEAD);
-        s->open(s,BODY);
-            s->open(s,H1);
-                s->segment_text(s,"This is a text");
-            s->close(s,H1);
-            s->open(s,P);
-                s->segment_format(s,"This is a formated  text  %s","aaaa");
-            s->close(s,P);
-
-        s->close(s,BODY);
-    s->close(s,HTML);
-    
-    return cweb_send_rendered_CTextStack(s,200);
+    char *teste = malloc(100);
+    strcpy(teste, "Hello World");
+    return cweb_send_text_cleaning_memory(teste,200);
 }
 
-int main(int argc, char const *argv[]){
- cweb_run_server(
-    8080,
-    main_sever,
-    CWEB_DEFAULT_TIMEOUT,
-    CWEB_DEFAULT_MAX_BODY,
-    CWEB_DANGEROUS_SINGLE_PROCESS
-    );
-}
+CWEB_START_MACRO(5000, main_sever)
+
