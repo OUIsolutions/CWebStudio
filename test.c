0381a6eb4fe7a6a756781388332ec10f98c21239
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-26 17:06:35 -0300

    att

--- test.c
+++ test.c
@@ -5,8 +5,26 @@
 int executions = 0;
 
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
-    request->represent(request);
-    return  cweb_send_file("a.out",CWEB_AUTO_SET_CONTENT,200);
+        const char *lang = "en";
+    const char *text = "text exemple";
+    struct CTextStack *s = newCTextStack(CTEXT_LINE_BREAKER, CTEXT_SEPARATOR);
+
+
+    s->$open(s,HTML,"lang=\"%s\"",lang);
+        s->open(s,HEAD);
+     
+        s->close(s,HEAD);
+        s->open(s,BODY);
+            s->open(s,H1);
+                s->segment_text(s,"This is a text");
+            s->close(s,H1);
+            s->open(s,P);
+                s->segment_format(s,"This is a formated  text  %s",text);
+            s->close(s,P);
+
+        s->close(s,BODY);
+    s->close(s,HTML);
+    return cweb_send_rendered_CTextStack_cleaning_memory(s,200);
     
 }
 
