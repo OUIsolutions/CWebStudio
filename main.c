f8d74c8c5a112dec6fbfa7dd406c61eb3e6fec11
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-26 14:51:38 -0300

    at

--- main.c
+++ main.c
@@ -1,34 +1,22 @@
-
-#include "CWebStudio/CwebStudioMain.h"
-
+#include "CWebStudio.h"
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
 
-    struct CTextStack *s  = newCTextStack(CTEXT_LINE_BREAKER,CTEXT_SEPARATOR);
-  
-    s->$open(s,HTML,"lang=\"%s\"","en");
-        s->open(s,HEAD);
-        
-        s->close(s,HEAD);
-        s->open(s,BODY);
-            s->open(s,H1);
-                s->segment_text(s,"This is a text");
-            s->close(s,H1);
-            s->open(s,P);
-                s->segment_format(s,"This is a formated  text  %s","aaaa");
-            s->close(s,P);
+    unsigned char *body = request->content;
+    int size = request->content_length;
 
-        s->close(s,BODY);
-    s->close(s,HTML);
+    //parse with cJson the body 
+
+    cJSON *json = cJSON_Parse(body);
+    cJSON *name = cJSON_GetObjectItemCaseSensitive(json, "name");
+    cJSON *age = cJSON_GetObjectItemCaseSensitive(json, "age");
+
+
+    printf("Name: %s\n", name->valuestring);
+    printf("Age: %d\n", age->valueint);
+
+
+    return cweb_send_text("Hello World", 200);
     
-    return cweb_send_rendered_CTextStack(s,200);
 }
 
-int main(int argc, char const *argv[]){
- cweb_run_server(
-    8080,
-    main_sever,
-    CWEB_DEFAULT_TIMEOUT,
-    CWEB_DEFAULT_MAX_BODY,
-    CWEB_DANGEROUS_SINGLE_PROCESS
-    );
-}
+CWEB_START_MACRO(5001, main_sever);
