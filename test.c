346761f2a670e065712bff667c206bda304ac7a3
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-27 16:17:46 -0300

    fix url encoded problens

--- test.c
+++ test.c
@@ -5,16 +5,13 @@
 int executions = 0;
 
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
-    char *content = request->content;
-    int content_size = request->content_length;
-
-   
+    request->represent(request);
     return cweb_send_text("aaa",200);
 }
 
 int main(){
     
-    /*
+    
       cweb_run_server(
         8080,
         main_sever,
@@ -22,12 +19,7 @@ int main(){
         CWEB_DEFAULT_MAX_BODY,
         CWEB_DANGEROUS_SINGLE_PROCESS
     );
-    */
-
-    char *decoded = private_cweb_convert_url_encoded_text("ddd=abcd%25%24%23ssss");
-    printf("%s\n",decoded);   
     
-
     
 
 }
