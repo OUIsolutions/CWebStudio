7706b7c82afa766824368084b283bea192072def
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-05-12 09:20:31 -0300

    q55

--- CWebStudio/server/request_execution.c
+++ CWebStudio/server/request_execution.c
@@ -1,27 +1,28 @@
 
+
 void private_cweb_execute_request(
     int socket,
-    size_t max_body_size,
-    struct CwebHttpResponse *(*request_handler)(struct CwebHttpRequest *request))
-            {
+    struct CwebHttpResponse *(*request_handler)(struct CwebHttpRequest *request)
+    ){
     cweb_print("Parsing Request\n");
-    struct CwebHttpRequest *request = cweb_request_constructor();
+    struct CwebHttpRequest *request = cweb_request_constructor(socket);
 
-    int result = request->parse_http_request(
-            request,
-            socket,
-            max_body_size
-    );
-
+    int result = request->parse_http_request(request);
+    
     if(result == INVALID_HTTP){
         cweb_print("Invalid HTTP Request\n");
-        private_cweb_send_error_mensage("Invalid HTTP Request",400,socket);
+        request->free(request);
         return;
     }
 
-    if(result == MAX_BODY_SIZE){
-        cweb_print("Max body size \n");
-        private_cweb_send_error_mensage("Max Request size Exceded",400,socket);
+    if(result == READ_ERROR){
+        cweb_print("Read Error \n");
+        request->free(request);
+        return;
+    }
+    if(result == MAX_HEADER_SIZE){
+        cweb_print("Max Header Size\n");
+        request->free(request);
         return;
     }
 
@@ -29,34 +30,67 @@ void private_cweb_execute_request(
     cweb_print("Request method: %s\n", request->method);
     cweb_print("Request url: %s\n", request->url);
 
+
+
     struct CwebHttpResponse *response;
-    response = request_handler(request);
+    #ifndef CWEB_NO_STATIC
+        response = private_cweb_generate_static_response(request);
+        if(response == NULL){
+            response = request_handler(request);
+        }
+    #else
+        response = request_handler(request);
+    #endif
+
     cweb_print("executed client lambda\n");
 
 
+    //means that the main function respond nothing
     if (response == NULL){
-        response = cweb_send_text(
-            "Error 404",
-            404);
-    };
+
+        #ifndef CWEB_NO_STATIC
+
+            char *formated_html = cweb_load_string_file_content("static/404.html");
+            if(formated_html != NULL){
+                response = cweb_send_var_html_cleaning_memory(
+                        formated_html,
+                        404);
+            }
+            else{
+                response = cweb_send_text(
+                        "Error 404",
+                        404
+                );
+            }
+
+        #else
+
+            response = cweb_send_text(
+                    "Error 404",
+                    404
+            );
+
+        #endif
+
+
+    }
 
     char *response_str = response->generate_response(response);
     cweb_print("Response created\n");
 
     send(socket, response_str, strlen(response_str), MSG_NOSIGNAL);
 
-    // Enviando conteúdo byte a byte
 
+    //envia todo o content de uma vez 
+
+    // Enviando conteúdo byte a byte
     if (response->exist_content)
     {
         size_t sent = 0;
         while (sent < response->content_length)
         {
             size_t chunk_size = response->content_length - sent;
-            if (chunk_size > max_body_size)
-            {
-                chunk_size = max_body_size;
-            }
+          
             ssize_t res = send(socket, response->content + sent, chunk_size, MSG_NOSIGNAL);
             if (res < 0)
             {
@@ -65,13 +99,13 @@ void private_cweb_execute_request(
             sent += res;
         }
     }
-
+    
     free(response_str);
     response->free(response);
     request->free(request);
     cweb_print("Cleared memory\n");
-    return;
 }
+
 
 void private_cweb_send_error_mensage( const char*mensage,int status_code, int socket)
 {
