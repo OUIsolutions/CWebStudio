5a95607c01bb41123677571aee70cad35c02b28a
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-05-13 22:59:35 -0300

    att

--- test_body_cloud.c
+++ test_body_cloud.c
@@ -1,15 +1,19 @@
+#define CWEB_DEBUG
 #include "CWebStudio.h"
-void write_binary_file(char *path, unsigned char *content, int size)
-{
-    FILE *file = fopen(path, "wb");
-    fwrite(content, sizeof(unsigned char), size, file);
-    fclose(file);
-}
-
 
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
    return cweb_send_text("Hello World",200);
     
 }
 
-CWEB_START_MACRO(5001, main_sever);
+int main(){
+
+    
+
+        for(int i=3000;i< 4000;i++){
+            cweb_run_server(i,main_sever,CWEB_DEFAULT_TIMEOUT,CWEB_DANGEROUS_SINGLE_PROCESS);
+    }
+ 
+ 
+    return 0;
+}
