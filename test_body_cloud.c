a82ad16927d6766bc384965083267f7e791772d7
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-05-14 00:30:14 -0300

    att

--- test_body_cloud.c
+++ test_body_cloud.c
@@ -8,9 +8,7 @@ struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
 
 int main(){
 
-    
-
-        for(int i=3000;i< 4000;i++){
+    for(int i=3000;i< 4000;i++){
             cweb_run_server(i,main_sever,CWEB_DEFAULT_TIMEOUT,CWEB_DANGEROUS_SINGLE_PROCESS);
     }
  
