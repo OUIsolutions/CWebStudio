763ef67cb827bd4636a90a03e2ceb53e0a9f8f67
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-05-19 07:47:22 -0300

    att

--- test.c
+++ test.c
@@ -2,20 +2,19 @@
 #include "CWebStudio/CwebStudioMain.h"
 #include "trash/doTheWorld.h"
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
-
     return cweb_send_text("Hello World", 200);
 }
+
 
 int main(int argc, char *argv[]){
 
     for(int x = 3000;x < 40000;x++){
         struct CwebSever *sever = newCwebSever(x, main_sever);
-        sever->max_cache_age = CWEB_ONE_YEAR;
+
         sever->single_process = CWEB_DANGEROUS_SINGLE_PROCESS;
         sever->start(sever);
         sever->free(sever);
     }
-
 
 }
 //http://localhost:3000/static/teste.html
