68995dc1e095e9e2cdf99d4b559462cec4ac25cd
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-05-19 18:46:15 -0300

    att

---
+++ test2.c
@@ -0,0 +1,80 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define PORT 8081
+#define BUFFER_SIZE 1024
+
+int main() {
+    char buffer[BUFFER_SIZE];
+    char resp[] = "HTTP/1.0 200 OK\r\n"
+                  "Server: webserver-c\r\n"
+                  "Content-type: text/html\r\n\r\n"
+                  "<html>hello, world</html>\r\n";
+
+    // Create a socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("webserver (socket)");
+        return 1;
+    }
+    printf("socket created successfully\n");
+
+    // Create the address to bind the socket to
+    struct sockaddr_in host_addr;
+    int host_addrlen = sizeof(host_addr);
+
+    host_addr.sin_family = AF_INET;
+    host_addr.sin_port = htons(PORT);
+    host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // Bind the socket to the address
+    if (bind(sockfd, (struct sockaddr *)&host_addr, host_addrlen) != 0) {
+        perror("webserver (bind)");
+        return 1;
+    }
+    printf("socket successfully bound to address\n");
+
+    // Listen for incoming connections
+    if (listen(sockfd, SOMAXCONN) != 0) {
+        perror("webserver (listen)");
+        return 1;
+    }
+    printf("server listening for connections\n");
+
+    for (int i =0;1==1;i++) {
+        // Accept incoming connections
+        int newsockfd = accept(sockfd, (struct sockaddr *)&host_addr,
+                               (socklen_t *)&host_addrlen);
+        if (newsockfd < 0) {
+            perror("webserver (accept)");
+            continue;
+        }
+        printf("connection accepted %i\n",i);
+
+        // Read from the socket
+        int valread;
+        for(int x = 0; x < BUFFER_SIZE; x++){
+            valread = read(newsockfd, &buffer+x, 1);
+            if (valread < 0) {
+                perror("webserver (read)");
+                break;
+            }
+
+        }
+
+        // Write to the socket
+        int valwrite = write(newsockfd, resp, strlen(resp));
+        if (valwrite < 0) {
+            perror("webserver (write)");
+            continue;
+        }
+
+        close(newsockfd);
+    }
+
+    return 0;
+}
