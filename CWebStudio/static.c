2571cafb8ddb7484bb217c901cb3eaf9ff43dcb4
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-05-18 22:38:10 -0300

    att

--- CWebStudio/static.c
+++ CWebStudio/static.c
@@ -1,7 +1,82 @@
 
+#include "CWebStudio/CwebStudioMain.h"
 
 
+char * private_cweb_change_smart_cache(const char *content){
 
+    struct CTextStack *code = newCTextStack("","");
+    struct CTextStack *buffer_pattern = newCTextStack("","");
+    struct CTextStack *src = newCTextStack("","");
+
+    unsigned long content_size = strlen(content);
+    const char *entry_pattern = "\"smart_cache:/static/";
+    unsigned long entry_pattern_len = strlen(entry_pattern);
+
+    int entry_founds = 0;
+    bool found_entry = false;
+
+    for(int i = 0; i < content_size; i++){
+
+        char current = content[i];
+        buffer_pattern->format(buffer_pattern,"%c",current);
+
+        if(found_entry){
+
+            //means its cancel the operation
+            if( current == '\n'){
+                code->text(code,buffer_pattern->rendered_text);
+                buffer_pattern->restart(buffer_pattern);
+                src->restart(src);
+                found_entry = false;
+                entry_founds = 0;
+                continue;
+            }
+
+            //means its getts the src
+            if(current != '"'){
+                src->format(src,"%c",current);
+                continue;
+            }
+
+            if(current == '"'){
+                //means its gthe end of src
+                // printf("src=%s\n",src->rendered_text);
+                char filename[2000];
+                sprintf(filename,"static/%s",src->rendered_text);
+                struct stat file_stat;
+                long last_mofication = 0;
+                if (stat(filename, &file_stat) == 0) {
+                    last_mofication = file_stat.st_mtime;
+                }
+                code->format(code,"\"/static?path=%s&update=%i\"",filename,last_mofication);
+
+                buffer_pattern->restart(buffer_pattern);
+                src->restart(src);
+                found_entry = false;
+                entry_founds = 0;
+                continue;
+            }
+        }
+
+        if(entry_founds +1  == entry_pattern_len){
+            found_entry = true;
+            continue;
+        }
+
+        if(current == entry_pattern[entry_founds]){
+            entry_founds+=1;
+            continue;
+        }
+
+        //means didnt get the  pattern
+        code->text(code,buffer_pattern->rendered_text);
+        buffer_pattern->restart(buffer_pattern);
+        entry_founds = 0;
+
+    }
+
+    return code->rendered_text;
+}
 
 struct CwebHttpResponse * private_cweb_treat_five_icon(struct CwebHttpRequest *request){
 
@@ -61,21 +136,28 @@ struct CwebHttpResponse * private_cweb_generate_static_response(struct CwebHttpR
         }
 
         char *securyt_path = cweb_replace_string(full_path,"../","");
+        int size;
+        bool is_binary;
+        unsigned char *content = cweb_load_any_content(securyt_path,&size,&is_binary);
 
 
-            
-        struct CwebHttpResponse * response = cweb_send_file(securyt_path,CWEB_AUTO_SET_CONTENT,200);
+        if(!is_binary){
+            char *new_content = private_cweb_change_smart_cache((char*)content);
+            free(content);
+            content = (unsigned char*)new_content;
+        }
+
+        char *content_type  = (char*)cweb_generate_content_type(securyt_path);
+
+        struct CwebHttpResponse * response = cweb_send_any_cleaning_memory(content_type,size,content,200);
         if(max_cache_age > 0){
             char response_code[40] = "";
             sprintf(response_code, "public max-age=%ld", max_cache_age);
             response->add_header(response,"Cache-Control:", response_code);
         }
-        
         free(securyt_path);
         return response;
     }
     return NULL;
-
-
 
 }
--- test.c
+++ test.c
@@ -1,76 +1,8 @@
-#include "CWebStudio.h"
-#include "trash/doTheWorld.h"
-
-
-char * change_smart_cache(const char *content){
-
-    struct CTextStack *code = newCTextStack("","");
-    struct CTextStack *buffer_pattern = newCTextStack("","");
-    struct CTextStack *src = newCTextStack("","");
-
-
-    unsigned long content_size = strlen(content);
-
-    const char *entry_pattern = "smart_cache:/)";
-
-    unsigned long entry_pattern_len = strlen(entry_pattern);
-    const char *out_pattern  = "\"";
-    unsigned long out_pattern_size = strlen(out_pattern);
-
-    int entry_founds = 0;
-    bool found_entry = false;
-
-    for(int i = 0; i < content_size; i++){
-
-        char current = content[i];
-        buffer_pattern->format(buffer_pattern,"%c",current);
-
-        if(found_entry && current != '"'){
-            //means its getting the src
-            src->format(src,"%c",current);
-            continue;
-        }
-
-
-        if(found_entry){
-            //means its gthe end of src
-           // printf("src=%s\n",src->rendered_text);
-
-            buffer_pattern->free(buffer_pattern);
-            buffer_pattern = newCTextStack("","");
-            src->free(src);
-            src = newCTextStack("","");
-            found_entry = false;
-            entry_founds = 0;
-            continue;
-        }
-
-
-        if(entry_founds +1  == entry_pattern_len){
-            found_entry = true;
-            src->format(src,"%c",current);
-            continue;
-        }
-
-        if(current == entry_pattern[entry_founds]){
-            entry_founds+=1;
-            continue;
-        }
+#include "CWebStudio/CwebStudioMain.h"
 
 
 
-        //means didnt get the  pattern
-        code->text(code,buffer_pattern->rendered_text);
-        buffer_pattern->free(buffer_pattern);
-        buffer_pattern = newCTextStack("","");
-        entry_founds = 0;
-
-    }
-    printf("%s",code->rendered_text);
-
-}
 int main(){
-    char *teste = dtw_load_string_file_content("static/teste.html");
-    char *result = change_smart_cache(teste);
-    printf("%s\n",result);
+
+   printf("%s\n",result);
 }
