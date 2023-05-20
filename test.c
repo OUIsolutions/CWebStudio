572f4ce8d04dfdcce21d8ea7e7f86a2c42a22674
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-05-20 19:32:14 -0300

    at

--- test.c
+++ test.c
@@ -1,25 +1,25 @@
 #define CWEB_DEBUG
 #include "CWebStudio/CwebStudioMain.h"
-
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
 
+    if(actual_request > 3){
+        request->free(request);
+        exit(0);
+    }
     const char *lang = "en";
     const char *text = "text exemple";
     struct CTextStack *s = newCTextStack(CTEXT_LINE_BREAKER, CTEXT_SEPARATOR);
-
 
     s->$open(s,HTML,"lang=\"%s\"",lang);
         s->open(s,HEAD);
             char *fav_icon_link = smart_static_ref("favicon.ico");
             s->auto$close(s,LINK,"rel=\"shortcut icon\" href=\"%s\"",fav_icon_link);
             free(fav_icon_link);
-
         s->close(s,HEAD);
         s->open(s,BODY);
             char *img_link = smart_static_ref("captura2.png");
             s->auto$close(s,IMG,"src=\"%s\" width=\"200px\"",img_link);
             free(img_link);
-
             s->open(s,H1);
                 s->segment_text(s,"This is a text");
             s->close(s,H1);
@@ -29,13 +29,15 @@ struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
 
         s->close(s,BODY);
     s->close(s,HTML);
+ 
     return cweb_send_rendered_CTextStack_cleaning_memory(s,200);
 }
 
 int main(int argc, char *argv[]){
 
    
-     struct CwebSever *sever = newCwebSever(5003, main_sever);
+     struct CwebSever *sever = newCwebSever(5002, main_sever);
+     sever->single_process = true;
      sever->start(sever);
      sever->free(sever);
 }
