4e661321bc837e2a21cfe1c7150602f2b75ab5d9
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-03 21:10:49 -0300

    at

--- build.py
+++ build.py
@@ -1,1 +1,9 @@
 
+
+import CToolKit as ct
+from os import listdir,remove
+
+
+
+STARTER  = f'CWebStudio/CwebStudioMain.h'
+ct.generate_amalgamated_code(STARTER,'CWebStudio.h')
--- test.c
+++ test.c
@@ -1,10 +1,24 @@
+#include "CWebStudio/CwebStudioMain.h"
 
-
-#include "CWebStudio.h"
-
-struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
-
-    return cweb_send_text("Hello World", 200);
+void write_binary_file(char *path, unsigned char *content, int size)
+{
+    FILE *file = fopen(path, "wb");
+    fwrite(content, sizeof(unsigned char), size, file);
+    fclose(file);
 }
 
-CWEB_START_MACRO(5001, main_sever);
+
+struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
+    int two_mega_bytes = 7097152;
+    request->read_content(request, two_mega_bytes);
+    unsigned char *body = request->content;
+    char *name = request->get_param(request, "name");
+    int size = request->content_length;
+
+    write_binary_file(name, body, size);
+
+    return cweb_send_text("File Written", 200);
+
+}
+
+CWEB_START_MACRO(5003, main_sever);
