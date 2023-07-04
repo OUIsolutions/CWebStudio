f0d2468eb72b188c4bb237052089bca99a035d4b
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-03 21:35:27 -0300

    attt

--- test.c
+++ test.c
@@ -1,16 +1,11 @@
 #include "CWebStudio.h"
-
-
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
 
-    struct CwebDict *query_paramns = request->params;
-    for(int i = 0; i < query_paramns->size; i++){
-        struct CwebKeyVal *key_val = query_paramns->keys_vals[i];
-        char *key = key_val->key;
-        char *value = key_val->value;
-        printf("%s : %s\n", key, value);
-    }
-    return cweb_send_text("Hello World", 200);
+    return cweb_send_file(
+        "2.png",
+        CWEB_AUTO_SET_CONTENT,
+        200
+    );
     
 }
 
