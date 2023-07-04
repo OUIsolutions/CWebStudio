a76a295d9dfd250b160f0885851aff6ebdf16896
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-03 23:45:46 -0300

    at

--- teste.py
+++ teste.py
@@ -1,8 +1,8 @@
 import requests
 text = ''
-for x in range(900):
-    text+='r'
+for x in range(4000):
+    text+='w'
     
-r = requests.get(f'http://localhost:5002/',headers={text:text})
+r = requests.get(f'http://localhost:5001/{text}')
 
 print(r.content)
