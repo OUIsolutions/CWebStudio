ed54cd5be68f617eed0dff8118f360ddb07d930d
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-04 00:05:58 -0300

    att

--- test.c
+++ test.c
@@ -1,12 +1,27 @@
 #include "CWebStudio.h"
+
 struct CwebHttpResponse *main_sever(struct CwebHttpRequest *request ){
 
-    return cweb_send_file(
-        "2.png",
-        CWEB_AUTO_SET_CONTENT,
-        200
-    );
-    
+    const char *lang = "en";
+    const char *text = "text exemple";
+    struct CTextStack *s = newCTextStack(CTEXT_LINE_BREAKER, CTEXT_SEPARATOR);
+
+
+    s->$open(s,HTML,"lang=\"%s\"",lang);
+        s->open(s,HEAD);
+     
+        s->close(s,HEAD);
+        s->open(s,BODY);
+            s->open(s,H1);
+                s->segment_text(s,"This is a text");
+            s->close(s,H1);
+            s->open(s,P);
+                s->segment_format(s,"This is a formated  text  %s",text);
+            s->close(s,P);
+
+        s->close(s,BODY);
+    s->close(s,HTML);
+    return cweb_send_rendered_CTextStack_cleaning_memory(s,200);
 }
 
-CWEB_START_MACRO(5004, main_sever);
+CWEB_START_MACRO(5000, main_sever)
