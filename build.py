7ddec01f5b81686098c7282deeb6ffa7b002d68c
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-13 22:27:36 -0300

    att

--- build.py
+++ build.py
@@ -4,10 +4,11 @@ import Build.CToolKit as ct
 from Build.copile_all_exemples import copile_all_exemples
 from Build.full_folder_zip import zip_folder
 
-
+from os import remove
 STARTER  = f'CWebStudio/CwebStudioMain.h'
 ct.generate_amalgamated_code(STARTER,'CWebStudio_test.h')
 copile_all_exemples('examples')
+remove('CWebStudio_test.h')
 
 def modifier(text:str):
     return text.replace('../CWebStudio_test.h','CWebStudio.h')
