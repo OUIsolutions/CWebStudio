4e36133f8c08f1da249b62818d44722b870e78d4
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-09-23 23:51:47 -0300

    at

--- CWebStudio.h
+++ CWebStudio.h
@@ -6818,8 +6818,6 @@ void private_cweb_run_server_in_single_process(
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
 
-    char client_ip[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &(address.sin_addr), client_ip, INET_ADDRSTRLEN);
 
     // Vinculando o socket à porta especificada
     if (bind(port_socket, (struct sockaddr *)&address, sizeof(address)) < 0)
@@ -6865,6 +6863,8 @@ void private_cweb_run_server_in_single_process(
             (socklen_t *)&addrlen
         );
 
+        char client_ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(address.sin_addr), client_ip, INET_ADDRSTRLEN);
 
 
         cweb_print("----------------------------------------\n");
@@ -7025,8 +7025,6 @@ void private_cweb_run_server_in_multiprocess(
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
 
-    char client_ip[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &(address.sin_addr), client_ip, INET_ADDRSTRLEN);
 
     // Vinculando o socket à porta especificada
     if (bind(port_socket, (struct sockaddr *)&address, sizeof(address)) < 0){
@@ -7076,6 +7074,8 @@ void private_cweb_run_server_in_multiprocess(
             (socklen_t *)&addrlen
         );
 
+        char client_ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(address.sin_addr), client_ip, INET_ADDRSTRLEN);
 
         cweb_print("----------------------------------------\n");
         cweb_print("Executing request:%lld\n", cweb_actual_request);
