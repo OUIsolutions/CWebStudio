73bf554c3fe23852387808f9aeb2d8ba17b5677b
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-04 20:42:07 -0300

    at

--- CWebStudio/definition.h
+++ CWebStudio/definition.h
@@ -3,7 +3,6 @@
 #include "extras/extras.c"
 #include "request/request.c"
 #include "request/request_parser.c"
-#include "response/response_functions/response_functions.c"
 #include "string_array/string_array.c"
 #include "dict/definition.h"
 #include "response/definition.h"
