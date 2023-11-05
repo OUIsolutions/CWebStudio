a76192d14b43e1ebe360b10c4529e5122fe811c0
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-05 01:09:05 -0300

    at

--- CWebStudio.h
+++ CWebStudio.h
@@ -6596,47 +6596,60 @@ CwebHttpResponse * private_cweb_generate_static_response(struct CwebHttpRequest 
         return private_cweb_treat_five_icon();
     }
 
-    int  base_route_size = (int)strlen("/static");
-    int min_size = base_route_size +2;
-
-
-    if(strlen(request->route) < min_size){
-        return NULL;
-    }
 
     if(!cweb_starts_with(request->route,"/static")){
         return  NULL;
     }
 
-    char *full_path = request->route;
-
-    full_path+= base_route_size;
+    char *full_path = NULL;
 
 
-    char *path = CwebHttpRequest_get_param(request,"path");
-    if(path != NULL){
-        full_path = path;
+    char *param_path = CwebHttpRequest_get_param(request,"path");
+
+    if(param_path){
+        full_path = param_path;
     }
 
-    char *securyt_path = cweb_replace_string(full_path,"../","");
+    if(!param_path){
+        full_path = request->route;
+        int  base_route_size = (int)strlen("/static");
+        int min_size = base_route_size +2;
 
+        if(strlen(full_path) < min_size){
+            return NULL;
+        }
+        full_path+= base_route_size;
+    }
+
+    const int MAX_PATH = 20;
+    if(strlen(full_path) > MAX_PATH){
+        return NULL;
+    }
+
+    char formated_full_path[2000] = {0};
+    sprintf(formated_full_path,"%s/%s",cweb_static_folder,full_path);
+
+    char *security_path = cweb_replace_string(formated_full_path,"../","");
     int size;
     bool is_binary;
-    unsigned char *content = cweb_load_any_content(securyt_path,&size,&is_binary);
+    unsigned char *content = cweb_load_any_content(security_path,&size,&is_binary);
 
     if(content == NULL){
 
-        char not_found_html_page_path[1000] ={0};
+        char not_found_html_page_path[1100] ={0};
         sprintf(not_found_html_page_path,"%s/404.html",cweb_static_folder);
         char *not_found_html_page = cweb_load_string_file_content(not_found_html_page_path);
         if(not_found_html_page != NULL){
+            free(security_path);
             return cweb_send_var_html_cleaning_memory(not_found_html_page,404);
 
         }
 
-        char mensage[100];
-        sprintf(mensage, "File not found: %s", securyt_path);
-        struct CwebHttpResponse* response =  cweb_send_text(mensage, CWEB_NOT_FOUND);
+        char menssage[1100] = {0};
+        sprintf(menssage,"File %s not found",security_path);
+        struct CwebHttpResponse* response =  cweb_send_text(menssage, CWEB_NOT_FOUND);
+        free(security_path);
+
         return response;
     }
 
@@ -6647,7 +6660,7 @@ CwebHttpResponse * private_cweb_generate_static_response(struct CwebHttpRequest 
         content = (unsigned char*)new_content;
     }
 
-    char *content_type  = (char*)cweb_generate_content_type(securyt_path);
+    char *content_type  = (char*)cweb_generate_content_type(security_path);
 
     struct CwebHttpResponse * response = cweb_send_any_cleaning_memory(content_type,size,content,200);
 
@@ -6661,7 +6674,7 @@ CwebHttpResponse * private_cweb_generate_static_response(struct CwebHttpRequest 
         }
     }
 
-    free(securyt_path);
+    free(security_path);
     return response;
 
 
