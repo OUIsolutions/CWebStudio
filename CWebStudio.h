663eb69168e2d16fb299a14f90c3e2c9fb945d3b
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-06 22:17:38 -0300

    at

--- CWebStudio.h
+++ CWebStudio.h
@@ -82,11 +82,11 @@ SOFTWARE.
 
 #define CTextScope(s,t)\
 ctext_open(s, t);\
-for(int i = 0; i < 1; ctext_close(s, t), ++i)
+for(int snaunduwwqwetjsdvda = 0; snaunduwwqwetjsdvda < 1; ctext_close(s, t), ++snaunduwwqwetjsdvda)
 
 #define CText$Scope(s,t, ...)\
 CTextStack_$open(s,t,__VA_ARGS__);\
-for(int i = 0; i < 1; ctext_close(s, t), ++i)
+for(int snaunduwwqwetjsdvda = 0; snaunduwwqwetjsdvda < 1; ctext_close(s, t), ++snaunduwwqwetjsdvda)
 
 
 
