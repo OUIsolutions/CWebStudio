e5cdcf77bc4f8b0391aee76cfd6c947b164301c8
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-28 19:11:02 -0300

    at

--- CWebStudio/declaration.h
+++ CWebStudio/declaration.h
@@ -8,7 +8,7 @@
 #include "dict/declaration.h"
 #include "response/declaration.h"
 #include "request/request.h"
-#include "static/static.h"
+#include "static/declaration.h"
 #include "cors/cors.h"
 #include "server/declaration.h"
 #include "namespace/declaration.h"
--- CWebStudio/static/static.h
+++
@@ -1,9 +0,0 @@
-
-
-char * cweb_smart_static_ref(const char *path);
-
-char * private_cweb_change_smart_cache(const char *content);
-
-CwebHttpResponse * private_cweb_treat_five_icon();
-
-CwebHttpResponse * private_cweb_generate_static_response(struct CwebHttpRequest *request,bool use_cache);
