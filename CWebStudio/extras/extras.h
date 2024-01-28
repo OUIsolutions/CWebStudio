e1d977dcce22b4e0a988d28217d0fa5cd1f8feb2
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-28 19:16:32 -0300

    att

--- CWebStudio/extras/extras.h
+++ CWebStudio/extras/extras.h
@@ -12,3 +12,4 @@ const char *cweb_generate_content_type(const char *file_name);
 char *private_cweb_convert_url_encoded_text(const char *text);
 
 
+bool private_cweb_is_string_from_point(const char *content, long content_size, const char *test_string, long test_string_size, long point);
