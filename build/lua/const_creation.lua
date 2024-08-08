dd625b1ff4bb6aecec8a12a54cdf8dab89d479f1
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-08-08 17:34:56 -0300

    att

--- build/lua/const_creation.lua
+++ build/lua/const_creation.lua
@@ -1,6 +1,10 @@
 local function create_js_consts()
-    local text = "const char * private_cweb_hydration_js_content"
-    local files, size = dtw.list_files_recursively(BIN_FILES .. "/hydration")
+    local text = 'const char * private_cweb_hydration_js_content = "'
+    local files, size = dtw.list_files_recursively(HYDRATION_FILES)
+    for i = 1, size do
+
+    end
+    text = text .. '"'
 end
 
 function Create_costs()
