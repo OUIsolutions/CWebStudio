5529b22b2fbe1e67bd8f66f66b3c99c6df60ad98
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-08-15 01:21:04 -0300

    at

--- src/functions/hydratation/bridge/bridge/entries/entries.c
+++ src/functions/hydratation/bridge/bridge/entries/entries.c
@@ -1,6 +1,14 @@
 
 #include "../uniq.definitions_requirements.h"
 
+void CWebHyDrationBridge_add_required_function(CWebHyDrationBridge *self,const char *function,...){
+    va_list  args;
+    va_start(args,function);
+    char *result = private_CWebHydration_format_vaarg(function,args);
+    va_end(args);
+    CwebStringArray_add_getting_ownership(self->entries_callbacks, result);
+}
+
 void CWebHyDrationBridge_get_element_by_id(CWebHyDrationBridge *self, const char * id){
 
 }
--- src/functions/hydratation/bridge/bridge/entries/entries.h
+++ src/functions/hydratation/bridge/bridge/entries/entries.h
@@ -1,4 +1,5 @@
 #include "../uniq.declaration_requirements.h"
 
+void CWebHyDrationBridge_add_required_function(CWebHyDrationBridge *self,const char *function,...);
 
 void CWebHyDrationBridge_get_element_by_id(CWebHyDrationBridge *self, const char * id);
