4be778967a4850a0fe38bb0da8d55cb165326527
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-08-15 11:34:38 -0300

    att

--- src/functions/hydratation/bridge/bridge/requirements/requirements.h
+++ src/functions/hydratation/bridge/bridge/requirements/requirements.h
@@ -4,10 +4,10 @@
 void CWebHyDrationBridge_add_required_function(CWebHyDrationBridge *self,const char *function,...);
 
 
-void CWebHyDrationBridge_add_required_text_by_id(CWebHyDrationBridge *self, const char * id);
+void CWebHyDrationBridge_add_required_by_id(CWebHyDrationBridge *self, const char * id);
 
 
-void CWebHyDrationBridge_add_required_text_number_by_id(CWebHyDrationBridge *self, const char * id);
+void CWebHyDrationBridge_add_required_number_by_id(CWebHyDrationBridge *self, const char * id);
 
 
 void CWebHyDrationBridge_add_required_input_by_id(CWebHyDrationBridge *self, const char * id);
