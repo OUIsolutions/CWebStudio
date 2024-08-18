e48703a10d519b6563718779efe19ce434c49a83
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-08-18 18:53:48 -0300

    att

--- bin/hydration/entries.js
+++ bin/hydration/entries.js
@@ -4,6 +4,11 @@ let private_cweb_entrie_clojure_constructors = {
   get_all_elements_by_query_selector: function (query_selector, set_content_key) {
   
     return function (args, content) {
+
+      if(!content[set_content_key]){
+        content[set_content_key] = []
+      }
+      
       let elements = document.querySelectorAll(query_selector);
       elements.forEach(function (element) {
         console.log(element);
