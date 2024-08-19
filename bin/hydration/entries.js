07c3098523d2d74a6edeb57d645fa321f7ed8d39
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-08-18 23:01:20 -0300

    att

--- bin/hydration/entries.js
+++ bin/hydration/entries.js
@@ -1,25 +1,27 @@
-let private_cweb_entrie_clojure_constructors = {
 
 
-  get_all_elements_by_query_selector: function (query_selector, set_content_key) {
-  
-    return function (args, content) {
 
-      if(!content[set_content_key]){
-        content[set_content_key] = []
-      }
-      
-      let elements = document.querySelectorAll(query_selector);
-      elements.forEach(function (element) {
-        console.log(element);
-      });
-    };
-  
-  },
+function private_cweb_get_elements_and_set_to_content(props) {
+  if (!props.content[props.set_content_key]) {
+    props.content[props.set_content_key] = [];
+  }
+  let content_array = props.content[props.set_content_key];
 
+  let elements = document.querySelectorAll(props.query_selector);
+  elements.forEach((element) => {
+    let finalvalue = undefined;
 
-  get_all_elements_by_query_selector_converting_to_nummber: function (
-    tag,
-    name,
-  ) {},
-};
+    if (element.tagName === "INPUT" || element.tagName == "TEXTAREA") {
+      finalvalue = element.value;
+    } else {
+      finalvalue = element.textContent;
+    }
+
+    if (props.convert_to_number) {
+      finalvalue = parseFloat(finalvalue);
+    }
+    if (finalvalue) {
+      content_array.unshift(finalvalue);
+    }
+  });
+}
