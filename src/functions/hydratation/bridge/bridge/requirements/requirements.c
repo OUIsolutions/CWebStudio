45da55fc11d3c6b8072b5d0c007a6a1362b1e46e
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-08-18 23:48:32 -0300

    att

--- src/functions/hydratation/bridge/bridge/requirements/requirements.c
+++ src/functions/hydratation/bridge/bridge/requirements/requirements.c
@@ -9,13 +9,7 @@ void CWebHyDrationBridge_add_required_function(CWebHyDrationBridge *self,const c
     CwebStringArray_add_getting_ownership(self->entries_callbacks, result);
 }
 
-void private_CWebHyDrationBridge_add_required_elements_by_query_selector(
-    CWebHyDrationBridge *self,
-    const char *key_name,
-    const char *query_selector,
-    bool convert_to_number
-){
-
+void private_CWebHyDrationBridge_add_required_elements_by_query_selector(CWebHyDrationBridge *self,const char *key_name,const char *query_selector,bool convert_to_number){
     const char *convert_to_number_str = "false";
     if(convert_to_number){
         convert_to_number = "true";
@@ -33,17 +27,74 @@ void private_CWebHyDrationBridge_add_required_elements_by_query_selector(
         query_selector,
         convert_to_number
     );
+
 }
 
-void CWebHyDrationBridge_add_required_elements_by_query_selector(
-    CWebHyDrationBridge *self,
-    const char *key_name,
-    const char *query_selector
-){
+
+
+void CWebHyDrationBridge_add_required_elements_by_query_selector(CWebHyDrationBridge *self,const char *key_name,const char *query_selector,...){
+
+    va_list  args;
+    va_start(args,query_selector);
+    char *formmated_query_selector = private_CWeb_format_vaarg(query_selector,args);
+    va_end(args);
+
     private_CWebHyDrationBridge_add_required_elements_by_query_selector(
         self,
         key_name,
-        query_selector,
+        formmated_query_selector,
         false
+    );
+    free(formmated_query_selector);
+}
+
+
+
+
+void CWebHyDrationBridge_add_required_elements_by_query_selector_converting_to_number(CWebHyDrationBridge *self,const char *key_name,const char *query_selector,...){
+
+    va_list  args;
+    va_start(args,query_selector);
+    char *formmated_query_selector = private_CWeb_format_vaarg(query_selector,args);
+    va_end(args);
+
+    private_CWebHyDrationBridge_add_required_elements_by_query_selector(
+        self,
+        key_name,
+        formmated_query_selector,
+        true
+    );
+    free(formmated_query_selector);
+}
+
+
+
+
+void CWebHyDrationBridge_add_required_elements_by_tag_name(CWebHyDrationBridge *self,const char *tag_name,const char*tag_value){
+
+    CWebHyDrationBridge_add_required_elements_by_query_selector(
+        self,
+        tag_value,
+        "[%s='%s']",
+        tag_name,
+        tag_value
+    );
+}
+
+
+void CWebHyDrationBridge_add_required_elements_by_class_name(CWebHyDrationBridge *self,const char*class_value){
+    CWebHyDrationBridge_add_required_elements_by_tag_name(
+        self,
+        "class",
+        class_value
+    );
+}
+
+
+void CWebHyDrationBridge_add_required_elements_by_id(CWebHyDrationBridge *self,const char*id_values){
+    CWebHyDrationBridge_add_required_elements_by_tag_name(
+        self,
+        "id",
+        id_values
     );
 }
--- src/functions/hydratation/bridge/bridge/requirements/requirements.h
+++ src/functions/hydratation/bridge/bridge/requirements/requirements.h
@@ -1,2 +1,18 @@
 
+
+
 #include "../uniq.declaration_requirements.h"
+
+void CWebHyDrationBridge_add_required_function(CWebHyDrationBridge *self,const char *function,...);
+
+void private_CWebHyDrationBridge_add_required_elements_by_query_selector(CWebHyDrationBridge *self,const char *key_name,const char *query_selector,bool convert_to_number);
+
+void CWebHyDrationBridge_add_required_elements_by_query_selector(CWebHyDrationBridge *self,const char *key_name,const char *query_selector,...);
+
+void CWebHyDrationBridge_add_required_elements_by_query_selector_converting_to_number(CWebHyDrationBridge *self,const char *key_name,const char *query_selector,...);
+
+void CWebHyDrationBridge_add_required_elements_by_tag_name(CWebHyDrationBridge *self,const char *tag_name,const char*tag_value);
+
+void CWebHyDrationBridge_add_required_elements_by_class_name(CWebHyDrationBridge *self,const char*class_value);
+
+void CWebHyDrationBridge_add_required_elements_by_id(CWebHyDrationBridge *self,const char*id_values);
