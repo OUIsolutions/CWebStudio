5c0da304475b4130a9ba1599964a34251ffce85f
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-08-20 13:32:58 -0300

    att

--- src/functions/hydratation/bridge/search_result/search_result.c
+++ src/functions/hydratation/bridge/search_result/search_result.c
@@ -1,8 +1,6 @@
 
 
 #include "../uniq.definitions_requirements.h"
-#include <time.h>
-
 
 
 int  CWebHyDrationBridge_get_total_avaialible_searchs(CWebHyDrationBridge *self){
@@ -69,4 +67,76 @@ bool CWebHyDrationBridge_search_exist(CWebHyDrationBridge *self,const char *name
         cJSON *search = cJSON_GetObjectItem(hydration->content,formmated_name);
         free(formmated_name);
         return search != NULL;
+}
+
+double CWebHyDrationBridge_get_double_from_first_element_of_search(CWebHyDrationBridge *self,const char *name,...){
+
+    va_list  args;
+    va_start(args,name);
+    char *formmated_name = private_CWeb_format_vaarg(name,args);
+    va_end(args);
+    CWebHyDrationSearchResult *search = CWebHyDrationBridge_get_search_by_name(self,"%s",formmated_name);
+    free(formmated_name);
+    if(search == NULL){
+        return  -1;
+    }
+    return CWebHyDrationSearchResult_get_double(search,0);
+}
+
+long CWebHyDrationBridge_get_long_from_first_element_of_search(CWebHyDrationBridge *self,const char *name,...){
+
+    va_list  args;
+    va_start(args,name);
+    char *formmated_name = private_CWeb_format_vaarg(name,args);
+    va_end(args);
+    CWebHyDrationSearchResult *search = CWebHyDrationBridge_get_search_by_name(self,"%s",formmated_name);
+    free(formmated_name);
+    if(search == NULL){
+        return  -1;
+    }
+    return CWebHyDrationSearchResult_get_long(search,0);
+}
+
+
+bool CWebHyDrationBridge_get_bool_from_first_element_of_search(CWebHyDrationBridge *self,const char *name,...){
+
+    va_list  args;
+    va_start(args,name);
+    char *formmated_name = private_CWeb_format_vaarg(name,args);
+    va_end(args);
+    CWebHyDrationSearchResult *search = CWebHyDrationBridge_get_search_by_name(self,"%s",formmated_name);
+    free(formmated_name);
+    if(search == NULL){
+        return  false;
+    }
+    return CWebHyDrationSearchResult_get_bool(search,0);
+}
+
+
+char* CWebHyDrationBridge_get_string_from_first_element_of_search(CWebHyDrationBridge *self,const char *name,...){
+
+    va_list  args;
+    va_start(args,name);
+    char *formmated_name = private_CWeb_format_vaarg(name,args);
+    va_end(args);
+    CWebHyDrationSearchResult *search = CWebHyDrationBridge_get_search_by_name(self,"%s",formmated_name);
+    free(formmated_name);
+    if(search == NULL){
+        return  NULL;
+    }
+    return CWebHyDrationSearchResult_get_string(search,0);
+}
+
+cJSON* CWebHyDrationBridge_get_cJSON_from_first_element_of_search(CWebHyDrationBridge *self,const char *name,...){
+
+    va_list  args;
+    va_start(args,name);
+    char *formmated_name = private_CWeb_format_vaarg(name,args);
+    va_end(args);
+    CWebHyDrationSearchResult *search = CWebHyDrationBridge_get_search_by_name(self,"%s",formmated_name);
+    free(formmated_name);
+    if(search == NULL){
+        return  NULL;
+    }
+    return CWebHyDrationSearchResult_get_cJSON(search,0);
 }
---
+++ src/functions/hydratation/bridge/search_result/search_result.h
@@ -0,0 +1,20 @@
+#include "../uniq.declaration_requirements.h"
+
+
+int  CWebHyDrationBridge_get_total_avaialible_searchs(CWebHyDrationBridge *self);
+
+CWebHyDrationSearchResult * CWebHyDrationBridge_get_search_by_index(CWebHyDrationBridge *self,int index);
+
+CWebHyDrationSearchResult * CWebHyDrationBridge_get_search_by_name(CWebHyDrationBridge *self,const char *name,...);
+
+bool CWebHyDrationBridge_search_exist(CWebHyDrationBridge *self,const char *name,...);
+
+double CWebHyDrationBridge_get_double_from_first_element_of_search(CWebHyDrationBridge *self,const char *name,...);
+
+long CWebHyDrationBridge_get_long_from_first_element_of_search(CWebHyDrationBridge *self,const char *name,...);
+
+bool CWebHyDrationBridge_get_bool_from_first_element_of_search(CWebHyDrationBridge *self,const char *name,...);
+
+char* CWebHyDrationBridge_get_string_from_first_element_of_search(CWebHyDrationBridge *self,const char *name,...);
+
+cJSON* CWebHyDrationBridge_get_cJSON_from_first_element_of_search(CWebHyDrationBridge *self,const char *name,...);
