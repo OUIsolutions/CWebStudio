2bee9a51fd9cc180f6f3a0de0688e8d5a53fecf4
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-08-25 22:48:37 -0300

    att

--- 2.lua
+++
@@ -1,9 +0,0 @@
-local final = ""
-
-final = final .. "<html>"
-final = final .. "<h3>o seu nome é "
-final = final .. nome
-final = final .. "</h3>"
-if idade > 18 then
-    final = final .. "vocé é de maior "
-end
