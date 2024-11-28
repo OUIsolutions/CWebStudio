01c49348ec1d549dd72bc7d6ccaa3a9946566571
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-11-27 22:52:03 -0300

    att

--- teste.c
+++ teste.c
@@ -1,13 +1,21 @@
+
 #include "src/one.c"
 CwebNamespace cweb;
 
 CwebHttpResponse *main_sever(CwebHttpRequest *request ){
-    return cweb.response.send_text("Hello World", 200);
+
+    if(strcmp(request->route, "/termina") == 0){
+        private_cweb_end_server = true;
+        return cweb.response.send_text(" finalizada", 200);
+    }
+
+    return cweb.response.send_file("imagem.png",CWEB_AUTO_SET_CONTENT,200);
 }
 
 int main(int argc, char *argv[]){
     cweb = newCwebNamespace();
-    struct CwebServer server = newCwebSever(5001, main_sever);
+    struct CwebServer server = newCwebSever(5003, main_sever);
+    //server.single_process = true;
     cweb.server.start(&server);
     return 0;
 }
