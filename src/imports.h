547ab16e2fe62794ab261f035134287b327db8aa
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-11-28 02:23:53 -0300

    test

--- src/imports.h
+++ src/imports.h
@@ -4,12 +4,7 @@
 #include <sys/wait.h>
 #endif
 
-#if defined(_WIN32)
-    #pragma comment(lib, "ws2_32.lib") // Linka a biblioteca Winsock
-    #include <winsock2.h>
-    #include <ws2tcpip.h>
 
-#endif
 
 #include <stdio.h>
 #include <sys/stat.h>
