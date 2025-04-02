f102cb41ef9aed1a91f165d31c3a349761ba2b1b
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-02 14:34:26 -0300

    att

--- src/consts/all.h
+++
@@ -1,8 +0,0 @@
-#ifndef PRIVATE_CWEB_HTTP_CONSTS_CONSTS
-#define PRIVATE_CWEB_HTTP_CONSTS_CONSTS
-
-#include "request.h"
-#include "response.h"
-#include "hydratation/all.h"
-
-#endif //PRIVATE_CWEB_HTTP_CONSTS_CONSTS
