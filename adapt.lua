2d76f028cac8ae35e857cb2c8d4057c8ac224ab8
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-02 16:10:53 -0300

    att

---
+++ adapt.lua
@@ -0,0 +1,17 @@
+
+local files = darwin.dtw.list_files_recursively("src/functions",true)
+
+for i=1,#files do
+    local current = files[i]
+    local path = darwin.dtw.newPath(current)
+    
+    local name = path.get_name()
+    
+    if darwin.dtw.ends_with(name,",c") then
+        path.set_name("fdefine."..name)        
+    end
+    if darwin.dtw.ends_with(name,",h") then
+        path.set_name("fdeclare."..name)        
+    end
+    darwin.dtw.move_any_overwriting(current,path.get_full_path())
+end 
