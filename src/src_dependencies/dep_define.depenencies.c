4f7fc35f6d19a91f8123e477af0436f1e6e32059
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-02 20:53:53 -0300

    att

---
+++ src/src_dependencies/dep_define.depenencies.c
@@ -0,0 +1,14 @@
+
+
+#if !defined(CWEB_MOCK_UNIVERSAL_SOCKET_DEFINE) && defined(CWEB_UNIVERSAL_SOCKET_DECLARATED)
+#include "../../dependencies/UniversalSocket.c"
+#endif 
+
+
+#if !defined(CWEB_MOCK_CJSON_DEFINE) && defined(CWEB_CJSON_DECLARATED)
+#include "../../dependencies/cJSON.c"
+#endif
+
+#if !defined(CWEB_MOCK_UNIVERSAL_GARBAGE_DEFINE) && defined(CWEB_UNIVERSAL_GARBAGE_DECLARATED)
+#include "../../dependencies/UniversalGarbage.c"
+#endif
