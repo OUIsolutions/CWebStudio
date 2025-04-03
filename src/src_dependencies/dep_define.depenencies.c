a3428426f2d8b18c21fce5a648d40e1b38791dee
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-02 21:11:07 -0300

    att

--- src/src_dependencies/dep_define.depenencies.c
+++ src/src_dependencies/dep_define.depenencies.c
@@ -5,6 +5,10 @@
 #include "../imports/imports.globals.h"
 //silver_chain_scope_end
 
+#if !defined(CWEB_MOCK_CTEXT_ENGINE_DEFINE) && defined(CWEB_CTEXT_ENGINE_DECLARATED)
+#include "../../dependencies/CTextEngine.c"
+#endif
+
 
 #if !defined(CWEB_MOCK_UNIVERSAL_SOCKET_DEFINE) && defined(CWEB_UNIVERSAL_SOCKET_DECLARATED)
 #include "../../dependencies/UniversalSocket.c"
